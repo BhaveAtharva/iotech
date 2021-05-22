56de01b7b8661ccf5c08599ea2629d586f35928f
BhaveAtharva <atharva.gopal@gmail.com>
2021-05-22 10:02:26 +0530

    Procfile

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from iotech import app, socketio
 
 if __name__ == '__main__':
     # app.run(debug=False)
-    socketio.run(app, host='127.0.0.1',debug=True)
+    socketio.run(app, host='127.0.0.1', port=5000)
